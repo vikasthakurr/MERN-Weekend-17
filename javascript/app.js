8f8b83283e55c2a39f65d9799a4b2740376feca0
vikasthakurr <vikaskumar20012001@gmail.com>
2025-11-23 16:27:06 +0530

    javascript

---
+++ javascript/app.js
@@ -0,0 +1,126 @@
+// var a = 10;
+
+// var double = (num) => {
+//   return num * 2;
+// };
+// var res1 = double(5);
+// console.log(res1);
+// var res2 = double(a);
+// console.log(res2);
+
+// var value = 20;
+// value = 50;
+// var value = "vikas";
+// console.log(value);
+// function test() {
+//   var value = 100;
+// }
+// {
+//   const value = 20;
+// }
+
+// console.log(value);
+
+// let a = 10;
+// // let a = 20;
+// a = 50;
+// const a = 10;
+// // const a=10;
+// a = 40;
+// console.log(a);
+
+// let salary = 12345678;
+// let salary2 = 1234;
+// console.log(salary);
+
+//data types
+
+// let a = 10;
+// // BigInt(s);
+// let b = 10.7;
+// let name = "vikas";
+// let isloggedIn = true;
+
+// let a = 10;
+// let b = a;
+// b = 30;
+// console.log(b);
+// console.log(a);
+
+// let obj = {
+//   name: "vikas",
+// };
+// let obj2 = obj;
+// obj2.name = "kumar";
+// console.log(obj2);
+// console.log(obj);
+
+// let obj1 = new Object();
+// obj1.name = "vikas";
+// obj1.age = 26;
+// obj1.isloggedIn = true;
+// console.log(obj1)
+
+// let arr = [10, 20, 30];
+// let arr2 = new Array(10, 20, 30);
+
+// function test() {}
+// test();
+
+// let greet = function () {
+//   console.log("hello world");
+// };
+
+// let greet = () => console.log("hello world");
+
+// (function greet() {
+//   console.log("hello world");
+// })();
+
+// (()=>{
+//     console.log("hello world");
+// })();
+
+// (()=>{
+//     console.log("hello world2");
+// })();
+
+//conditional
+
+let age = 17;
+
+// if (age > 18) {
+//   console.log("eligible for voting");
+// } else if (age == 18) {
+//   console.log("just eligible for voting but a teenager");
+// } else {
+//   console.log("not eligible for voting");
+// }
+// console.log(
+//   age > 18
+//     ? "eligible for voting"
+//     : age == 18
+//     ? "just eligible for voting but a teenager"
+//     : "not eligible for voting"
+// );
+
+// let day = 70;
+
+// switch (day) {
+//   case 1:
+//     console.log("monday");
+//     break;
+//   case 2:
+//     console.log("tuesday");
+//     break;
+//   case 5:
+//     console.log("friday");
+//     break;
+//   default:
+//     console.log("invalid day");
+//     break;
+// }
+
+let a = 10;
+let b = "10";
+console.log(a === b);
