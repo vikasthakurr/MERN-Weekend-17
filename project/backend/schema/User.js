d53828d92af8e1275dcae5555ffae30b8f69eab1
vikasthakurr <vikaskumar20012001@gmail.com>
2026-01-04 14:52:02 +0530

    schema

--- project/backend/schema/User.js
+++ project/backend/schema/User.js
@@ -1,1 +1,20 @@
-const UserSchema= new m
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
